955a95ad5b9f11e982d7808272eb6e813d94e1e9
Asif Ahamed <asifshezan7@gmail.com>
2022-12-14 20:22:09 +0600

    Route Create Update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,4 +17,5 @@ Route::post('register', [RegisterController::class, 'register']);
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
 
+    Route::resource('products', ProductController::class);
 });
